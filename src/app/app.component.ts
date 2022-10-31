1f471addf343f6deab103999f91c2edc81c2f4fd
NightmanCZ90 <novak.jan90@gmail.com>
2022-10-31 22:32:31 +0100

    feat: Create form using pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
